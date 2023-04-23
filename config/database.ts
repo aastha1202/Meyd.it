a1bcc816ed8188263a141a1ff0d2c91c143af98b
aastha1202 <99357921+aastha1202@users.noreply.github.com>
2023-04-23 20:56:40 +0530

    Update database.ts

--- config/database.ts
+++ config/database.ts
@@ -36,10 +36,10 @@ const databaseConfig: DatabaseConfig = {
     mysql: {
       client: 'mysql2',
       connection: {
-        host: Env.get('MYSQL_HOST','localhost'),
+        host: Env.get('MYSQL_HOST'),
         port: Env.get('MYSQL_PORT'),
-        user: Env.get('MYSQL_USER','root'),
-        password: Env.get('MYSQL_PASSWORD', ''),
+        user: Env.get('MYSQL_USER'),
+        password: Env.get('MYSQL_PASSWORD'),
         database: Env.get('MYSQL_DB_NAME'),
         ssl: {
           rejectUnauthorized: true,
