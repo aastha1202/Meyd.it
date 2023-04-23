5d2700ec5a5dc51f13c41fd3c84a7ec98d0647dd
aastha1202 <99357921+aastha1202@users.noreply.github.com>
2023-04-23 20:51:20 +0530

    Update database.ts

--- config/database.ts
+++ config/database.ts
@@ -41,7 +41,6 @@ const databaseConfig: DatabaseConfig = {
         user: Env.get('MYSQL_USER','root'),
         password: Env.get('MYSQL_PASSWORD', ''),
         database: Env.get('MYSQL_DB_NAME'),
-        connectTimeout: 50000,
         ssl: {
           rejectUnauthorized: true,
         }
